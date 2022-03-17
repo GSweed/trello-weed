9a22ee52ffb7b28a3c6c1ce392aa155584e8c54a
Giang Son <chukygiangson@gmail.com>
2022-03-17 17:27:25 +0700

    edit remove column

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -89,6 +89,26 @@ function BoardContent() {
     toggleOpenNewColumnForm()
   }
 
+  const onUpdateColumn = (newColumnToUpdate) =>{
+      const columnIdToUpdate = newColumnToUpdate.id
+
+      let newColumns = [...columns]
+      
+      const columnIndexToUpdate = newColumns.findIndex(i => i.id === columnIdToUpdate)
+
+      if(newColumnToUpdate._destroy){
+          newColumns.splice(columnIndexToUpdate, 1)
+      }else{
+        newColumns.splice(columnIndexToUpdate, 1, newColumnToUpdate)
+      }
+      let newBoard = {...board}
+      newBoard.columnOder = newColumns.map(c => c.id)
+      newBoard.columns = newColumns
+
+      setColumns(newColumns)
+      setBoard(newBoard)
+  }
+
   return (
     <div className='board-content'>
       <Container
@@ -105,7 +125,7 @@ function BoardContent() {
         >
           {columns.map((column, index) => (
             <Draggable key ={index}>
-                <Column  column = {column} onCardDrop = {onCardDrop}/>
+                <Column  column = {column} onCardDrop = {onCardDrop} onUpdateColumn={onUpdateColumn}/>
             </Draggable>
             
           ))}
--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,28 +1,80 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { Container, Draggable } from "react-smooth-dnd";
 import './Column.scss'
 import Card from '../Card/Card'
+import ConfirmModal from 'components/Common/ConfirmModal';
 import { mapOrder } from 'utilities/sorts'
+import { Dropdown, Form } from 'react-bootstrap';
+import { MODAL_ACTION_CONFIRM } from 'utilities/constants';
+import { saveContentAfterEnter, selectAllInlineText } from 'utilities/contentEdit';
 function Column(props) {
-  const { column, onCardDrop } = props
+  const { column, onCardDrop, onUpdateColumn } = props
   //sort card
   const cards = mapOrder(column.cards, column.cardOder, 'id')
+  const onConfirmModalAction = (type) => {
+    if(type === MODAL_ACTION_CONFIRM){
+        const newColumn = {
+          ...column,
+          _destroy: true,
+        }
+        onUpdateColumn(newColumn)
+    }
+
+    toggleShowConfirmModal()
+  }
+  //set state hien thi confirm modal
+  const [showConfirmModal, setShowConfirmModal] = useState(false)
+  const toggleShowConfirmModal = () =>{
+    setShowConfirmModal(!showConfirmModal)
+  }
+  const [columnTitle, setColumnTitle] = useState('')
+  const handleColumnTitleChange = useCallback((e) => setColumnTitle(e.target.value),[])
+  useEffect(() =>{
+    setColumnTitle(column.title)
+  },[column.title])
+
   
+  const handleColumnTitleBlur = () =>{
+    const newColumn = {
+      ...column,
+      title: columnTitle,
+    }
+    onUpdateColumn(newColumn)
+  }
+
   return (
     <div className='column'>
-      <header className='column-drag-handle'>{column.title}</header>
+      <header className='column-drag-handle'>
+        <div className='column_title'>
+        <Form.Control 
+              size='sm' 
+              type='text' 
+              className='weed-content-edit'
+              value={columnTitle}
+              spellCheck="false"
+              onClick={selectAllInlineText}
+              onChange={handleColumnTitleChange}
+              onBlur={handleColumnTitleBlur}
+              onKeyDown = {saveContentAfterEnter}
+              onMouseDown={e => e.preventDefault()}
+              />  
+        </div>
+        <div className='column_dropdown_actions'>
+          <Dropdown>
+            <Dropdown.Toggle size='sm' className='dropdown-btn' id="dropdown-basic"/>
+
+            <Dropdown.Menu >
+              <Dropdown.Item >Add Card</Dropdown.Item>
+              <Dropdown.Item onClick={toggleShowConfirmModal}>Remove Column</Dropdown.Item>
+              <Dropdown.Item >Move all card in this column</Dropdown.Item>
+              <Dropdown.Item >Archive all card in this column</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+        
+      </header>
       <div className='Card-list'>
       <Container
-                    
-                    // onDragStart={e => console.log("drag started", e)}
-                    // onDragEnd={e => console.log("drag end", e)}
-                    // onDragEnter={() => {
-                    //   console.log("drag enter:", column.id);
-                    // }}
-                    // onDragLeave={() => {
-                    //   console.log("drag leave:", column.id);
-                    // }}
-                    // onDropReady={p => console.log('Drop ready: ', p)}
                     dropPlaceholder={{                      
                       animationDuration: 150,
                       showOnTop: true,
@@ -48,6 +100,12 @@ function Column(props) {
        <i className='fa fa-plus icon'/> Add another card
        </div>
       </footer>
+      <ConfirmModal
+        show={showConfirmModal}
+        onAction={onConfirmModalAction}
+        title='remove Column'
+        content={`Are you sure to remove column ${column.title}`}
+      />
     </div>
   )
 }
